660f0ab266e81ce036e619e7a67c497851f4107f
Daniel1227k <dc049709@gmail.com>
2024-01-10 20:21:48 -0600

    finish 1 day

---
+++ day1/app.js
@@ -0,0 +1,45 @@
+//*1.- Escribe un comentario de una sola línea que diga "los comentarios pueden hacer que el código sea legible"
+
+//los comentarios hacen que todo sea mas legible
+
+//*2- Escribe otro comentario que diga, "Bienvenido a 30DaysOfJavaScript"
+//Welcome a 30 Days of JavaScript
+
+//*3.- Escribe un comentario multilínea que diga, "Los comentarios pueden hacer el código legible, fácil de usar e informativo"
+/*Los comentarios pueden hacer el cogigo mas legible, facil de usar e informativo */
+
+//*4.- Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
+let nameUser = 'Daniel';
+let isOld = false;
+let married;
+let haveMoney = null;
+
+//*5.-Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+console.log(typeof nameUser);
+console.log(typeof isOld);
+console.log(typeof married);
+console.log(typeof haveMoney);
+
+//* Declara cuatro variables sin asignar valores
+
+let year;
+let minutes;
+let oldYear;
+let nameCat;
+
+//* Declara cuatro variables con valores asignados
+year = 2024;
+minutes = 16;
+oldYear = 21;
+nameCat = 'Copito';
+
+//* Declara variables para almacenar su nombre, apellido, estado civil, país y edad en múltiples líneas
+let myName;
+let lastName;
+let isMarried;
+let country;
+let age;
+
+//* Declara dos variables miEdad y tuEdad y asígneles valores iniciales y regístrese en la consola del navegador.
+let myAge = 21;
+let yourAge = 18;
